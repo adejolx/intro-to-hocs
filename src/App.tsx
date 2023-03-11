df755fc2d1fc5cd29f21fe0252b82a9d12496a3f
adejolx <recursr.dev@gmail.com>
2023-03-11 03:23:34 +0100

    refactor: replace InputContainer with SignUpForm
    Formik library was introduced rendering InputContainer redundant

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import BaseInput from "./components/BaseInput";
+import SignupForm from "./components/Form";
 
 import { FlexFooter } from "./components/HOCs/FlexFooter";
 import InputContainer from "./components/InputContainer";
 
 function App() {
-  const [input, setInput] = useState("");
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  };
   return (
     <div className="App wrapper">
-      <InputContainer>
-        <BaseInput
-          type={"text"}
-          display={"block"}
-          onChange={handleChange}
-          value={input}
-          size={{
-            height: "50",
-            width: "100%",
-          }}
-          placeholder="Enter a value"
-          statusMessage={undefined}
-          isVerifiable={false}
-          isShaded={false}
-          isTrimmable={false}
-          className="stack-s"
-        />
-        <FlexFooter wrap={"nowrap"}>
-          <div>Mininum Amount: {input}</div>
-          <div>Available balance: {input}</div>
-        </FlexFooter>
-      </InputContainer>
+      <SignupForm />
     </div>
   );
 }
