8c023151c5906de7df9f72b96ca55b250bd63f0c
adejolx <recursr.dev@gmail.com>
2023-03-13 09:23:48 +0100

    update: App component
    Also removed non-existing imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import BaseInput from "./components/BaseInput";
 import SignupForm from "./components/Form";
-
-import { FlexFooter } from "./components/HOCs/FlexFooter";
-import InputContainer from "./components/InputContainer";
 
 function App() {
   return (
